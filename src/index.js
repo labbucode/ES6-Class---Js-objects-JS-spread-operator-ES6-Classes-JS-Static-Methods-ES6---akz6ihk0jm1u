685d91f31222d905d8711477eece2d6b296be89c
labbucode <110982212+labbucode@users.noreply.github.com>
2023-01-06 01:54:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-
+construction (url,method='GET'){
+this.url= url;
+this.method= method;
+this.#secure= url.startWith('https')
+}
+isSecure(){
+return this.secure;
+}
+updateUrl(newURL){
+this.url = newURL
+this.#secure = newURL('https')
+}
+}
 
 module.exports = { API }
